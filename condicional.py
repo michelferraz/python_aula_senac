9ebc00d9bfe8f04740fb47def967c67d8b5587d6
Michel Ferraz <michelferraz.cunha@gmail.com>
2023-11-30 11:47:28 -0300

    condicional, escopo, variaveis, input

--- condicional.py
+++ condicional.py
@@ -1,6 +1,6 @@
 #condição de True ou False (sempre escritos com a primeira letra maiuscula)
-# if(se) e ou else (senão)
-
+# if(se) e ou else (senão), elif (pergunta dentro de outra pergunta)
+"""
 a = False
 print(type(a))
 if a:
@@ -25,3 +25,35 @@ if nome == "Jose":
     print("É igual")
 else:
     print("É diferente")
+
+
+nota1, nota2, nota3 = 10, 5, 1
+media = (nota1 + nota2 + nota3) / 3
+print(media)
+if media >= 7:
+    print("Aprovado")
+elif media <5:
+    print("Reprovado")
+else:
+    print("Apavorado")
+"""
+
+
+#exercicio, digite o nome do aluno e as notas
+
+nome = input ("Ok, Digite seu nome : ")
+nota1 = int(input ("Agora digite sua nota de Matemática : "))
+nota2 = int(input ("Digite sua nota de Português : "))
+nota3 = int(input ("E por fim, digite sua nota de História : "))
+
+media = (nota1 + nota2 + nota3) / 3
+print(media)
+
+if media >= 7:
+    print("Parabens, você foi Aprovado!")
+elif media >=3:
+    print("Você vai precisar frequentar uma Recuperação")
+elif media >=1:
+    print("Infelizmente você foi Reprovado")
+else:
+    print("MEEEEU DEEEEUS, entregou em branco!")
