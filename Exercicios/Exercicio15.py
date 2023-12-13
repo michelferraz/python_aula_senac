8033e6e4f3564696446129ec5c14ab0dc72cb307
Michel Ferraz <michelferraz.cunha@gmail.com>
2023-12-13 11:28:33 -0300

    att

--- Exercicios/Exercicio15.py
+++ Exercicios/Exercicio15.py
@@ -3,9 +3,7 @@
 
 numero = int(input("Digite um número: "))
 
-if numero <= (100:200):
+if numero > 100 and numero < 200: #aqui se usa o and para comparar os valores
     print("Este número está entre os valores de 100 a 200.")
-elif numero >=100:
-        print("Este número está entre os valores de 100 a 200.")
 else:
     print("Este número não está entre os valores de 100 a 200.")
